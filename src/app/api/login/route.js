30c35c95a5e241b00264118e65d54c2d5d10971b
soum-ik <sarkarsoumik215@gmail.com>
2024-03-05 02:52:40 +0600

    stric

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -7,7 +7,7 @@ export async function POST(req, res) {
     const { password, email } = reqbody;
     if (email === "test@gmail.com" && password === "200") {
       let token = await CreateToken(email);
-      const cookieString = `token = ${token}; Max-age = 7200; Secure; HttpOnly; path=/; SameSite=Strict`;
+      const cookieString = `token = ${token}; Max-age = 7200; Secure; HttpOnly; path=/;`;
       console.log(cookieString, "cookie");
 
       return NextResponse.json(
@@ -29,5 +29,13 @@ export async function POST(req, res) {
 
 export async function GET(req, res) {
   // Clearing the cookie by setting an expired date
-  return NextResponse.json({ status: true, message: "Logout Success" }, { status: 200, headers: { "Set-Cookie": "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/" } });
+  return NextResponse.json(
+    { status: true, message: "Logout Success" },
+    {
+      status: 200,
+      headers: {
+        "Set-Cookie": "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/",
+      },
+    }
+  );
 }

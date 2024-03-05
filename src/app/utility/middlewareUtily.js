5c0a1e24f67b91247dc941577bcd45c354d4616d
soum-ik <sarkarsoumik215@gmail.com>
2024-03-05 12:56:54 +0600

    cookie

--- src/app/utility/middlewareUtily.js
+++ src/app/utility/middlewareUtily.js
@@ -1,12 +1,10 @@
-import { NextResponse } from "next/server"; 
+import { NextResponse } from "next/server";
 import { VerifyToken } from "./JwtTokenHelper";
 
 export async function CheckCookieAuth(request) {
+  let token = await request.cookies.get("token");
+  console.log(token);
   try {
-         // Retrieve the token from the cookies
-    let token = request.cookies.get("token");
-
-    // Ensure that the token is not null or undefined
     if (!token) {
       throw new Error("Token not found in cookies");
     }
--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,10 @@
-import { NextResponse } from "next/server";
-import { VerifyToken } from "./app/utility/JwtTokenHelper";
+// import { NextResponse } from "next/server";
+// import { VerifyToken } from "./app/utility/JwtTokenHelper";
+
+import { CheckCookieAuth } from "./app/utility/middlewareUtily";
 
 export async function middleware(request) {
   if (request.nextUrl.pathname.startsWith("/private")) {
-    try {
-      // Retrieve the token from the cookies
-      let token = request.cookies.get("token");
-
-      // Ensure that the token is not null or undefined
-      if (!token) {
-        throw new Error("Token not found in cookies");
-      }
-
-      let payload = await VerifyToken(token["value"]);
-
-      const requestHeaders = new Headers(request.headers);
-      requestHeaders.set("email", payload["email"]);
-      return NextResponse.next({
-        request: { headers: requestHeaders },
-      });
-    } catch (e) {
-      console.log(e, "tk");
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+    return CheckCookieAuth(request);
   }
 }
